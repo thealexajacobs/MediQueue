19b89eba74b03be7e212da611f03fa84d0c312a1
Blessing Bamiduro <bamiduroblessing996@gmail.com>
2026-06-19 01:12:02 +0100

    Add breathing space between topbar and tabs on mobile

--- components/dashboard/DashboardShell.tsx
+++ components/dashboard/DashboardShell.tsx
@@ -191,7 +191,7 @@ export function DashboardShell({ clinicName: propClinicName, clinicLogo, clinicI
           onSettingsClick={() => setIsSettingsOpen(true)}
         />
 
-      <div className="flex items-center border-b border-border/10 px-4 sm:px-6 min-w-0">
+      <div className="flex items-center border-b border-border/10 px-4 pt-3 sm:px-6 sm:pt-0 min-w-0">
         <QueueTabs
           queues={queues}
           selectedQueueId={selectedQueueId}
